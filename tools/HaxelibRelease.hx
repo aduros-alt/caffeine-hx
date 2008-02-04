5becf218ad668bd5df6d2af16926d6f8520f9a4e
damonsbane <damonsbane@5be69f5d-4a45-0410-b242-ad22a5f24ccc>
2008-02-04 01:10:39 +0000

    Tools
    git-svn-id: http://caffeine-hx.googlecode.com/svn/trunk@3 5be69f5d-4a45-0410-b242-ad22a5f24ccc

---
+++ tools/HaxelibRelease.hx
@@ -0,0 +1,28 @@
+
+class HaxelibRelease.hx {
+	static var project : String = "caffeine-hx";
+	static var desc : String = "Extended library for haxe";
+	static var maintainer : String = "Madrok";
+	static var version : String;
+	static var releasenotes : String;
+
+
+	public static function main() {
+		if(neko.FileSystem.exists(".snv"))
+			error("Must be run from an exported version of caffeine-hx");
+	}
+
+	public static function fileContents() : String {
+		var s = "<project name=\""+project+"\" url=\"http://code.google.com/p/caffeine-hx\" license=\"BSD\">\n";
+		s += "    <user name=\""++maintainer"\"/>\n";
+		s += "    <description>"+desc+"</description>\n";
+		s += "    <version name=\""+version+"\">"+releasenotes+"</version>\n";
+		s += "</project>\n";
+		return s;
+	}
+
+	public static function error( s : String ) {
+		neko.Lib.println(s);
+		neko.Sys.exit(1);
+	}
+}
