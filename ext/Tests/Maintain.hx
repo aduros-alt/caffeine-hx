1bed8b266083de8d285a19be9857f2d6f548a1a5
damonsbane <damonsbane@5be69f5d-4a45-0410-b242-ad22a5f24ccc>
2008-02-07 21:01:22 +0000

    Added ext/Tests/Maintain.hx which builds, runs and cleans all tests. Still needs some work for automatically building a root index.html file that will link to all the subdirectories. Would be nice if it could launch the browser automatically as well.
    git-svn-id: http://caffeine-hx.googlecode.com/svn/trunk@58 5be69f5d-4a45-0410-b242-ad22a5f24ccc

---
+++ ext/Tests/Maintain.hx
@@ -0,0 +1,162 @@
+
+class Builder { 
+	function print(s : String) {
+		neko.Lib.print(s);
+	}
+	public function new() {
+		var dirs = Maintain.getDirs();
+		var owd = neko.Sys.getCwd();
+		for(d in dirs) {
+			neko.Sys.setCwd(d);
+			print("Building " + d + "\n");
+			var rv = neko.Sys.command("haxe", ["build.hxml"]);
+			neko.Sys.setCwd(owd);
+			if(rv != 0) {
+				if(!Maintain.doContinue)
+					Maintain.exitError("");
+			}
+		}
+	}
+}
+
+class Runner {
+	function print(s : String) {
+		neko.Lib.print(s);
+	}
+	public function new() {
+		var dirs = Maintain.getDirs();
+		var owd = neko.Sys.getCwd();
+		for(d in dirs) {
+			neko.Sys.setCwd(d);
+			print("\n----------------------------------------\n");
+			print(d + "...");
+			if(neko.FileSystem.exists("test.n")) {
+				print("\n----------------------------------------\n");
+				var rv = neko.Sys.command("neko", ["test.n"]);
+				if(rv != 0) {
+					if(!Maintain.doContinue)
+						Maintain.exitError("");
+				}
+			}
+			else {
+				print("skipped. No neko file.");
+				print("\n----------------------------------------\n");
+			}
+			neko.Sys.setCwd(owd);
+		}
+	}
+}
+
+class Cleaner {
+	function print(s : String) {
+		neko.Lib.print(s);
+	}
+
+	public static function getFiles() {
+		var a = neko.FileSystem.readDirectory(".");
+		var b : Array<String> = new Array();
+		for(d in a) {
+			var ereg = ~/^[test]/;
+			if(!ereg.match(d)) {
+				var ereg = ~/\.n$/;
+				if(!ereg.match(d))
+					continue;
+			}
+			if(!neko.FileSystem.isFile(d)) {
+				Maintain.exitError("file " + d + " is not a regular file!");
+			}
+			b.push(d);
+		}
+		return b;
+	}
+
+	public function new() {
+		var dirs = Maintain.getDirs();
+		var owd = neko.Sys.getCwd();
+		for(d in dirs) {
+			print("Cleaning " + d + "\n");
+			neko.Sys.setCwd(d);
+			var flist = getFiles();
+			for(f in flist) {
+				print("..."+f+"\n");
+				var rv = neko.Sys.command("rm", [f]);
+				if(rv != 0) {
+					Maintain.exitError("Error removing " + f);
+				}
+			}
+			neko.Sys.setCwd(owd);
+		}
+	}
+}
+
+class Maintain {
+	static public var doBuild : Bool;
+	static public var doRun : Bool;
+	static public var doClean : Bool;
+	static public var doContinue : Bool;
+
+	public static function usage() {
+		var s = "neko maintain.n\n";
+		s+= "\t--build\tBuild all tests\n";
+		s += "\t--run       Run neko tests\n";
+		s += "\t--clean     Remove all compiled files\n";
+		s += "\t--continue  Continue on errors\n";
+		return s;
+	}
+
+	public static function getDirs() {
+		var a = neko.FileSystem.readDirectory(".");
+		var b : Array<String> = new Array();
+		for(d in a) {
+			if(!neko.FileSystem.isDirectory(d))
+				continue;
+			var ereg = ~/^[a-z]/;
+			if(!ereg.match(d))
+				continue;
+			b.push(d);
+		}
+		return b;
+	}
+
+	public static function main() {
+		if(!neko.FileSystem.exists("Templates") || !neko.FileSystem.exists("hash"))
+			exitError("You don't seem to be running in the Tests directory");
+		if(neko.Sys.args().length == 0)
+			exitError(usage());
+
+		for(arg in neko.Sys.args()) {
+			if(arg == "--help" || arg == "-h") {
+				neko.Lib.println(usage());
+				neko.Sys.exit(0);
+			}
+			if(arg == "--build")
+				doBuild = true;
+			else if ( arg == "--run" )
+				doRun = true;
+			else if ( arg == "--clean" )
+				doClean = true;
+			else if ( arg == "--continue" )
+				doContinue = true;
+			else
+				exitError("Invalid command " + arg);
+		}
+
+		if(doBuild) {
+			var p = new Builder();
+		}
+		if(doRun) {
+			var p = new Runner();
+		}
+		if(doClean) {
+			var p = new Cleaner();
+		}
+
+	}
+
+	public static function exitError(s:String) {
+		neko.Lib.println(s);
+		neko.Sys.exit(100);
+	}
+
+}
+
