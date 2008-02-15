28d1d1c323096b5d56b5f5af4a347ec312a31879
damonsbane <damonsbane@5be69f5d-4a45-0410-b242-ad22a5f24ccc>
2008-02-15 23:09:36 +0000

    Additional test vectors for SHA1
    git-svn-id: http://caffeine-hx.googlecode.com/svn/trunk@80 5be69f5d-4a45-0410-b242-ad22a5f24ccc

--- ext/Tests/hash/HashTest.hx
+++ ext/Tests/hash/HashTest.hx
@@ -1,8 +1,55 @@
 import hash.Md5;
 import hash.Sha1;
-#if !neko
 import hash.Sha256;
+
+class Vectors {
+	public static function Sha1() {
+		var message : Array<String> = new Array();
+		var hash : Array<String> = new Array();
+		message.push("");
+		hash.push("DA39A3EE5E6B4B0D3255BFEF95601890AFD80709");
+
+		message.push("a");
+		hash.push("86F7E437FAA5A7FCE15D1DDCB9EAEAEA377667B8");
+
+		message.push("abc");
+		hash.push("A9993E364706816ABA3E25717850C26C9CD0D89D");
+
+		message.push("message digest");
+		hash.push("C12252CEDA8BE8994D5FA0290A47231C1D16AAE3");
+
+		message.push("abcdefghijklmnopqrstuvwxyz");
+		hash.push("32D10C7B8CF96570CA04CE37F2A19D84240D3A89");
+
+		message.push("abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq");
+		hash.push("84983E441C3BD26EBAAE4AA1F95129E5E54670F1");
+
+		message.push("ABCDEFGHIJKLMNOPQRSTUVWXYZZabcdefghijklmnopqrstuvwxyzz0123456789");
+		hash.push("32ebf96b6fe3561d2368873b45c82af1d101dad3");
+
+		var sb = new StringBuf();
+		for(x in 0...8)
+			sb.add("1234567890");
+		message.push(sb.toString());
+		hash.push("50ABF5706A150990A08B2C5EA40FA0E585554732");
+
+		// null bits - wrong hash..
+		//message.push(ByteStringTools.nullString(30));
+		//hash.push("2759FBD73570D43A3954F54F02D39042B1B91A76");
+#if neko
+		//1 million times "a"
+		sb = new StringBuf();
+		for(x in 0...100000)
+			sb.add("aaaaaaaaaa");
+		message.push(sb.toString());
+		hash.push("34AA973CD4C4DAA4F61EEB2BDBAD27316534016F");
 #end
+
+		for(x in 0...hash.length)
+			hash[x] = hash[x].toLowerCase();
+		return { messages: message, hashes : hash };
+	}
+}
 
 class Sha1TestFunctions extends haxe.unit.TestCase {
 
@@ -21,7 +68,18 @@ class Sha1TestFunctions extends haxe.unit.TestCase {
 		);
 	}
 
-
+	public function testVectors() {
+		var o = Vectors.Sha1();
+		for(x in 0...o.messages.length) {
+			try {
+				assertEquals(o.hashes[x], Sha1.encode(o.messages[x]));
+			}
+			catch(e:Dynamic) {
+				trace("Error on Sha1 Test Vector # " + Std.string(x+1));
+				throw currentTest;
+			}
+		}
+	}
 #if neko
 	public function testObjSha() {
 
@@ -63,13 +121,12 @@ class Sha1TestFunctions extends haxe.unit.TestCase {
 			"a9993e364706816aba3e25717850c26c9cd0d89d",
 			rv
 		);
-		
+
 	}
 #end
 
 }
 
-#if !neko
 class Sha256TestFunctions extends haxe.unit.TestCase {
 	public function testSha256() {
 		assertEquals(
@@ -78,7 +135,6 @@ class Sha256TestFunctions extends haxe.unit.TestCase {
 		);
 	}
 }
-#end
 
 class Md5TestFunctions extends haxe.unit.TestCase {
 	function testMd5() {
@@ -91,13 +147,11 @@ class Md5TestFunctions extends haxe.unit.TestCase {
 }
 
 class HashTest {
-	static function main() 
+	static function main()
 	{
 		var r = new haxe.unit.TestRunner();
 		r.add(new Sha1TestFunctions());
-#if !neko
 		r.add(new Sha256TestFunctions());
-#end
 		r.add(new Md5TestFunctions());
 		r.run();
 	}
