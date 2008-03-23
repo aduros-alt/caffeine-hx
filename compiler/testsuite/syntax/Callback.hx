04d292b82ac4129a60ce39427ac0c200d679be3e
damonsbane <damonsbane@5be69f5d-4a45-0410-b242-ad22a5f24ccc>
2008-03-23 22:12:21 +0000

    Added a class variable that should update during a call to f()
    git-svn-id: http://caffeine-hx.googlecode.com/svn/trunk@165 5be69f5d-4a45-0410-b242-ad22a5f24ccc

--- compiler/testsuite/syntax/Callback.hx
+++ compiler/testsuite/syntax/Callback.hx
@@ -3,26 +3,31 @@ package syntax;
 import unit.Assert;
 
 class Callback {
+	var counter : Int;
 	public function new() {}
-	
+
 	public function testCallback() {
+		counter = 0;
         var n = "haXe";
         var cc = callback(f, n);
 		Assert.equals("haXe", cc());
+		Assert.equals(1,counter);
         n = "Neko";
         Assert.equals("haXe", cc());
+		Assert.equals(2,counter);
     }
-	
+
 	// for comparison
-	public function testClosure() { 
+	public function testClosure() {
         var n = "haXe";
         var cc = function() { return n; };
 		Assert.equals("haXe", cc());
         n = "Neko";
         Assert.equals("Neko", cc());
     }
- 
+
     public function f(name:String) {
+		counter ++;
         return name;
     }
 }
