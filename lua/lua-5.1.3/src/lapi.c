758c5e965b5aee52491605200a11e0d78326599c
damonsbane <damonsbane@5be69f5d-4a45-0410-b242-ad22a5f24ccc>
2008-04-08 01:55:56 +0000

    Patched current reported bugs for Lua 5.1.3 http://www.lua.org/bugs.html
    git-svn-id: http://caffeine-hx.googlecode.com/svn/trunk@202 5be69f5d-4a45-0410-b242-ad22a5f24ccc

--- lua/lua-5.1.3/src/lapi.c
+++ lua/lua-5.1.3/src/lapi.c
@@ -93,15 +93,14 @@ void luaA_pushobject (lua_State *L, const TValue *o) {
 
 
 LUA_API int lua_checkstack (lua_State *L, int size) {
-  int res;
+  int res = 1;
   lua_lock(L);
-  if ((L->top - L->base + size) > LUAI_MAXCSTACK)
+  if (size > LUAI_MAXCSTACK || (L->top - L->base + size) > LUAI_MAXCSTACK)
     res = 0;  /* stack overflow */
-  else {
+  else if (size > 0) {
     luaD_checkstack(L, size);
     if (L->ci->top < L->top + size)
       L->ci->top = L->top + size;
-    res = 1;
   }
   lua_unlock(L);
   return res;
