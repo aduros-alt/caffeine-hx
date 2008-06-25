e9b0012eaa6596cdc5326aaaacd7f317ecf7e2fa
damonsbane <damonsbane@5be69f5d-4a45-0410-b242-ad22a5f24ccc>
2008-06-25 14:52:20 +0000

    Nicolas' version
    git-svn-id: http://caffeine-hx.googlecode.com/svn/trunk@302 5be69f5d-4a45-0410-b242-ad22a5f24ccc

--- neko/libs/std/date.c
+++ neko/libs/std/date.c
@@ -36,25 +36,14 @@ extern field id_s;
 extern field id_y;
 extern field id_d;
 
-static struct tm *get_localtime(time_t *d) {
-	struct tm * t;
 #ifdef NEKO_WINDOWS
-	t = localtime(d);
-#else
-	t = (time_t *)malloc(sizeof(struct tm));
-	if( t == NULL)
-		return NULL;
-	localtime_r(d, t);
-#endif
-	return t;
+static struct tm *localtime_r( time_t *t, struct tm *r ) {
+	struct tm *r2 = localtime(t);
+	if( r2 == NULL ) return NULL;
+	*r = *r2;
+	return r;
 }
-
-static void free_localtime(struct tm *t) {
-#ifndef NEKO_WINDOWS
-	if(t != NULL)
-		free(t);
 #endif
-}
 
 /**
 	date_now : void -> 'int32
@@ -121,18 +110,16 @@ static value date_new( value s ) {
 **/
 static value date_format( value o, value fmt ) {
 	char buf[128];
-	struct tm *t;
+	struct tm t;
 	time_t d;
 	val_check(o,int32);
 	if( val_is_null(fmt) )
 		fmt = alloc_string("%Y-%m-%d %H:%M:%S");
-	val_check(fmt,string);	
+	val_check(fmt,string);
 	d = val_int32(o);
-	t = get_localtime(&d);
-	if( t == NULL )
+	if( localtime_r(&d,&t) == NULL )
 		neko_error();
-	strftime(buf,127,val_string(fmt),t);
-	free_localtime(t);
+	strftime(buf,127,val_string(fmt),&t);
 	return alloc_string(buf);
 }
 
@@ -141,21 +128,19 @@ static value date_format( value o, value fmt ) {
 	<doc>Change the time of a date. Return the modified date</doc>
 **/
 static value date_set_hour( value o, value h, value m, value s ) {
-	struct tm *t;
+	struct tm t;
 	time_t d;
 	val_check(o,int32);
 	val_check(h,int);
 	val_check(m,int);
 	val_check(s,int);
 	d = val_int32(o);
-	t = get_localtime(&d);
-	if( t == NULL )
+	if( localtime_r(&d,&t) == NULL )
 		neko_error();
-	t->tm_hour = val_int(h);
-	t->tm_min = val_int(m);
-	t->tm_sec = val_int(s);
-	d = mktime(t);
-	free_localtime(t);
+	t.tm_hour = val_int(h);
+	t.tm_min = val_int(m);
+	t.tm_sec = val_int(s);
+	d = mktime(&t);
 	if( d == -1 )
 		neko_error();
 	return alloc_int32(d);
@@ -166,21 +151,19 @@ static value date_set_hour( value o, value h, value m, value s ) {
 	<doc>Change the day of a date. Return the modified date</doc>
 **/
 static value date_set_day( value o, value y, value m, value d ) {
-	struct tm *t;
+	struct tm t;
 	time_t date;
 	val_check(o,int32);
 	val_check(y,int);
 	val_check(m,int);
 	val_check(d,int);
 	date = val_int32(o);
-	t = get_localtime(&date);
-	if( t == NULL )
+	if( localtime_r(&date,&t) == NULL )
 		neko_error();
-	t->tm_year = val_int(y) - 1900;
-	t->tm_mon = val_int(m) - 1;
-	t->tm_mday = val_int(d);
-	date = mktime(t);
-	free_localtime(t);
+	t.tm_year = val_int(y) - 1900;
+	t.tm_mon = val_int(m) - 1;
+	t.tm_mday = val_int(d);
+	date = mktime(&t);
 	if( date == -1 )
 		neko_error();
 	return alloc_int32(date);
@@ -192,18 +175,16 @@ static value date_set_day( value o, value y, value m, value d ) {
 **/
 static value date_get_day( value o ) {
 	value r;
-	struct tm *t;
+	struct tm t;
 	time_t d;
 	val_check(o,int32);
 	d = val_int32(o);
-	t = get_localtime(&d);
-	if( t == NULL )
+	if( localtime_r(&d,&t) == NULL )
 		neko_error();
 	r = alloc_object(NULL);
-	alloc_field(r,id_y,alloc_int(t->tm_year + 1900));
-	alloc_field(r,id_m,alloc_int(t->tm_mon + 1));
-	alloc_field(r,id_d,alloc_int(t->tm_mday));
-	free_localtime(t);
+	alloc_field(r,id_y,alloc_int(t.tm_year + 1900));
+	alloc_field(r,id_m,alloc_int(t.tm_mon + 1));
+	alloc_field(r,id_d,alloc_int(t.tm_mday));
 	return r;
 }
 
@@ -213,18 +194,16 @@ static value date_get_day( value o ) {
 **/
 static value date_get_hour( value o ) {
 	value r;
-	struct tm *t;
+	struct tm t;
 	time_t d;
 	val_check(o,int32);
 	d = val_int32(o);
-	t = get_localtime(&d);
-	if( t == NULL )
+	if( localtime_r(&d,&t) == NULL )
 		neko_error();
 	r = alloc_object(NULL);
-	alloc_field(r,id_h,alloc_int(t->tm_hour));
-	alloc_field(r,id_m,alloc_int(t->tm_min));
-	alloc_field(r,id_s,alloc_int(t->tm_sec));
-	free_localtime(t);
+	alloc_field(r,id_h,alloc_int(t.tm_hour));
+	alloc_field(r,id_m,alloc_int(t.tm_min));
+	alloc_field(r,id_s,alloc_int(t.tm_sec));
 	return r;
 }
 
