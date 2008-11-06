6f1fa161fa0ab3f3c1394b34b2c25516e03fbc9e
damonsbane <damonsbane@5be69f5d-4a45-0410-b242-ad22a5f24ccc>
2008-11-06 10:49:14 +0000

    Flash 9/AS3 Weak Reference class
    git-svn-id: http://caffeine-hx.googlecode.com/svn/trunk@401 5be69f5d-4a45-0410-b242-ad22a5f24ccc

---
+++ ext2/as3/WeakReference.hx
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2008, The Caffeine-hx project contributors
+ * Original author : Russell Weir
+ * Contributors:
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE CAFFEINE-HX PROJECT CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE CAFFEINE-HX PROJECT CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package as3;
+
+#if flash9
+import flash.utils.TypedDictionary;
+
+/**
+	Stores a weak reference to an object. This class itself should be used as a strong
+	reference.
+	<code>
+		var wr : WeakReference;
+		function blah() {
+			wr = new WeakReference(new Foo());
+		}
+		function blah2() {
+			if(wr.get() != null)
+				trace("Foo still exists");
+		}
+	</code>
+
+**/
+class WeakReference {
+	var d : TypedDictionary<Dynamic,Dynamic>;
+
+	/**
+		Create a new weak reference to an existing object.
+	**/
+	public function new(o : Dynamic) {
+		d = new TypedDictionary(true);
+		d.set(o, null);
+	}
+
+	/**
+		Retrieves the object, or null if it has already been garbage collected
+	**/
+	public function get() : Dynamic {
+		for(o in d)
+			return o;
+		return null;
+	}
+}
+
+#end
