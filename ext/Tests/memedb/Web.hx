8cd378006de0ce1d105f5790fb2e5cebd20b39e1
damonsbane <damonsbane@5be69f5d-4a45-0410-b242-ad22a5f24ccc>
2009-06-13 20:40:50 +0000

    Removed files
    git-svn-id: http://caffeine-hx.googlecode.com/svn/trunk@559 5be69f5d-4a45-0410-b242-ad22a5f24ccc

--- ext/Tests/memedb/Web.hx
+++
@@ -1,5 +0,0 @@
-class Web {
-	public static function main() {
-		neko.Lib.println("Hello world from neko <b>embedded</b>");
-	}
-}
--- ext/Tests/memedb/books/Settings.hx
+++
@@ -1,6 +0,0 @@
-
-class Settings {
-	public static var DBNAME : String = "books-example";
-	public static var USER	: String = "sa";
-	public static var PASS : String = "password";
-}
